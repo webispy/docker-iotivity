d4f3a5d5129d4d6e5083f1c578507e1f305f3ce9
Inho Oh <webispy@gmail.com>
2018-01-31 17:39:54 +0900

    Add sample source
    Signed-off-by: Inho Oh <webispy@gmail.com>

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 [![Docker Pulls](https://img.shields.io/docker/pulls/webispy/iotivity.svg)](https://hub.docker.com/r/webispy/iotivity/)
 
 # docker-iotivity
+
+## build test
+
+```sh
+$ git clone https://github.com/webispy/docker-iotivity
+$ docker build docker-iotivity -t iotivity
+$ docker run -it --rm -v docker-iotivity/sample:/src iotivity bash -c "rm -rf src/build; mkdir src/build; cd src/build; cmake ..; make"
+$ cd docker-iotivity/sample/build
+$ ./myclient
+```
+
---
+++ sample/src/main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include <iotivity_config.h>
+#include <platform_features.h>
+#include <ocstack.h>
+#include <ocpayload.h>
+
+static OCStackApplicationResult on_discovery(void *ctx, OCDoHandle handle,
+		OCClientResponse *resp)
+{
+	if (!resp)
+		return OC_STACK_KEEP_TRANSACTION;
+
+	printf("Discovered %s:%d\n", resp->devAddr.addr, resp->devAddr.port);
+
+	return OC_STACK_KEEP_TRANSACTION;
+}
+
+static int do_discovery(void)
+{
+	OCCallbackData *cb;
+
+	cb = malloc(sizeof(OCCallbackData));
+	cb->cb = on_discovery;
+	cb->context = NULL;
+	cb->cd = NULL;
+
+	return OCDoRequest(NULL, OC_REST_DISCOVER, "/oic/res",
+			NULL, NULL, CT_DEFAULT,
+			OC_LOW_QOS, cb, NULL, 0);
+}
+
+int main(int argc, char *argv[])
+{
+	struct timespec timeout;
+
+	printf("\nIoTivity(%s) client\n", IOTIVITY_VERSION);
+
+	printf("Start IoTivity stack...\n");
+
+	if (OCInit(NULL, 0, OC_CLIENT) != OC_STACK_OK) {
+		printf("OCStack init error\n");
+		return -1;
+	}
+
+	do_discovery();
+
+	timeout.tv_sec = 0;
+	timeout.tv_nsec = 10000000L; /* 0.01 sec */
+
+	while (1) {
+		if (OCProcess() != OC_STACK_OK) {
+			printf("OCStack process error\n");
+			return -1;
+		}
+		nanosleep(&timeout, NULL);
+	}
+
+	if (OCStop() != OC_STACK_OK)
+		printf("OCStack process error\n");
+
+	return 0;
+}
